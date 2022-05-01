9c1d1cd26f6a10d649d8c718651afe492e24d4f0
DevinShelly <devin.shelly@gmail.com>
2022-05-01 00:30:55 -0400

    Update bet.js

--- bet.js
+++ bet.js
@@ -1,5 +1,5 @@
 class Bet{
-  constructor(date, sportsbook, event, wager, odds, returned)
+  constructor(date, sportsbook, event, wager, odds, returned, id)
   {
     this.date = date;
     this.sportsbook = sportsbook;
@@ -7,11 +7,12 @@ class Bet{
     this.wager = wager;
     this.odds = odds;
     this.returned = returned;
+    this.id = id;
   }
   
   get TSV()
   {
-    return this.date + "\t" + this.sportsbook + "\t" + this.event + "\t" + this.wager + "\t" + this.odds + "\t" + this.returned + "\n";
+    return this.date + "\t" + this.sportsbook + "\t" + this.event + "\t" + this.wager + "\t" + this.odds + "\t" + this.returned + "\t" + this.id + "\n";
   }
 }
 
